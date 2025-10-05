783cafdb05731579060cb5deaf90db8e8924cde9
Emile Steenkamp <emile@steenkamps.org>
2025-10-05 18:13:49 +0200

    Add exception annotation to define method

--- library/core/src/commonMain/kotlin/me/emilesteenkamp/orktstrator/definition/Define.kt
+++ library/core/src/commonMain/kotlin/me/emilesteenkamp/orktstrator/definition/Define.kt
@@ -1,9 +1,11 @@
 package me.emilesteenkamp.orktstrator.definition
 
 import me.emilesteenkamp.orktstrator.api.Orktstrator
+import me.emilesteenkamp.orktstrator.api.OrktstratorException
 import me.emilesteenkamp.orktstrator.api.State
 
 @Suppress("UNUSED")
+@Throws(OrktstratorException.DefinitionException::class)
 fun <TRANSIENT_STATE, FINALISED_STATE> Orktstrator.Companion.define(
     builder: OrktstratorDefinition<TRANSIENT_STATE, FINALISED_STATE>.() -> Unit,
 ): Orktstrator<TRANSIENT_STATE, FINALISED_STATE> where
